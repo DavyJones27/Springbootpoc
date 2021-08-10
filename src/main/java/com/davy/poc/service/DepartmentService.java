38bed80c61e319e3097e5967de0672317d658b5c
DavyJones27 <ajayyadavkol16@gmail.com>
2021-08-10 17:37:38 +0530

    add and removed unwanted file

---
+++ src/main/java/com/davy/poc/service/DepartmentService.java
@@ -0,0 +1,4 @@
+package com.davy.poc.service;
+
+public interface DepartmentService {
+}
---
+++ src/main/java/com/davy/poc/service/DepartmentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.davy.poc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+}
