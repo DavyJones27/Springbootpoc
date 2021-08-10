135fadc06a9840604cf0944783cf59e1afff2937
DavyJones27 <ajayyadavkol16@gmail.com>
2021-08-10 18:51:14 +0530

    getter done

--- src/main/java/com/davy/poc/controller/DepartmentController.java
+++ src/main/java/com/davy/poc/controller/DepartmentController.java
@@ -1,9 +1,21 @@
 package com.davy.poc.controller;
 
+import com.davy.poc.entity.Department;
+import com.davy.poc.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+   private DepartmentService departmentService;
 
+    @PostMapping("/department")
+    public Department saveDepartment(@RequestBody Department department){
+
+        return departmentService.saveDepartment(department);
+    }
 }
--- src/main/java/com/davy/poc/service/DepartmentService.java
+++ src/main/java/com/davy/poc/service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.davy.poc.service;
 
+import com.davy.poc.entity.Department;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
 }
