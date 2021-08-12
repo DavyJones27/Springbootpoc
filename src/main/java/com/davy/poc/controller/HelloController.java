a56c15247462fd7142c8d72c3c7124f132e9baf1
DavyJones27 <ajayyadavkol16@gmail.com>
2021-08-13 01:47:24 +0530

    complete

--- src/main/java/com/davy/poc/controller/HelloController.java
+++ src/main/java/com/davy/poc/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.davy.poc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @Value("${some.config}")
+    private String welcomeMessage;
+
+    //    @RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
     public String helloworld() {
-        return "Hello people jjj";
+        return welcomeMessage;
     }
 
 }
